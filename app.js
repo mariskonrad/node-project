539bc9f2a13a252968a41c20e411c985a005fc3b
Marisbel Konrad <marisbelk@gmail.com>
2023-04-23 21:50:05 -0300

    add validateAuth

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
 
 const framework = require('./framework')
+const users = require('./db/users')
 
 const app = framework()
 
+const sessions = {}
+const SESSION_ID = 'sessionId'
+
 app.get('/', (req, res) => {
+  // res.setHeader('Set-Cookie', `dog=toninho`)
   res.render('home')
 })
 
@@ -13,7 +18,68 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-  res.end()
+  const buffer = []
+
+  req.on('data', chunk => {
+    buffer.push(chunk)
+  })
+
+  req.on('end', () => {
+    const body = Buffer.concat(buffer).toString()
+    const params = new URLSearchParams(body)
+
+    const username = params.get('username')
+    const password = params.get('password')
+    if (!(username in users) && !users[username] === password) {
+      res.statusCode = 401
+      res.render('login')
+      return
+    }
+    const uuid = uuidv4()
+    sessions[uuid] = username
+    res.setHeader('Set-Cookie', `sessionId=${uuid}`)
+    res.render('profile')
+  })
 })
+
+app.get('/profile', (req, res) => {
+  if (validateAuth(req, res)) {
+    return
+  }
+  res.render('profile')
+})
+
+app.post('/logout', (req, res) => {
+  if (validateAuth(req, res)) {
+    return
+  }
+
+  res.setHeader('Set-Cookie', `sessionId=deleted; expires=Thu, 01 Jan 1970 00:00:00 GMT`)
+  res.render('home')
+})
+
+const parseCookie = req => {
+  const cookies = req.headers.cookie
+
+  return (
+    cookies?.split('; ').reduce((acc, curr) => {
+      currSplitted = curr.split('=')
+      acc[currSplitted[0]] = currSplitted[1]
+      return acc
+    }, {}) ?? {}
+  )
+}
+
+const validateAuth = (req, res) => {
+  const parsedCookies = parseCookie(req)
+
+  if (SESSION_ID in parsedCookies && parsedCookies[SESSION_ID] in sessions) {
+    return false
+  }
+
+  res.statusCode = 401
+  res.render('login')
+  return true
+}
 
 module.exports = app
---
+++ db/users.js
@@ -0,0 +1,6 @@
+const users = {
+  maris: '123',
+  toninho: '456'
+}
+
+module.exports = users
