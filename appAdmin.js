7727cbe3c40ad574028d2f283e4d9cdfc0b4f022
Marisbel Konrad <marisbelk@gmail.com>
2023-04-30 21:25:07 -0300

    remove admin app

--- appAdmin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const appAdmin = express()
-
-appAdmin.get('/', (req, res) => {
-  res.sendFile('views/admin.html', { root: __dirname })
-})
-
-module.exports = appAdmin
--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const app = require('./app')
-const appAdmin = require('./appAdmin')
 
 const hostname = '0.0.0.0'
 const port = 3000
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
-})
-
-const portAdmin = 3001
-
-appAdmin.listen(portAdmin, hostname, () => {
-  console.log(`Admin running at http://${hostname}:${portAdmin}/`)
 })
